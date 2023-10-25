22271f8a5003596aad61816ca3bfa760194b2dab
king <2066176114@qq.com>
2023-10-25 23:27:23 +0800

    整合用户登录注册模块
    Signed-off-by: king <2066176114@qq.com>

---
+++ hnit_ucenter/src/main/java/com/hnit/video/ucenter/config/UcenterConfig.java
@@ -0,0 +1,25 @@
+package com.hnit.video.ucenter.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.hnit.ucenter.mapper")
+@ComponentScan(basePackages = {"com.hnit"})
+public class UcenterConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setDbType(DbType.MYSQL);
+        paginationInnerInterceptor.setOverflow(true);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
+    }
+}
